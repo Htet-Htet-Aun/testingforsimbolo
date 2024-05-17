c5abd17659ec014f360af62f7d55f5859c19078a
Htet-Htet-Aun <rposy988@gmail.com>
2024-05-17 21:25:58 +0630

    Add files via upload

---
+++ streamlit_demo (1).py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+#number = st.number_input("Insert a number")
+#st.write("The current number is ", number)
+
+#st.write(7*number)
+
+st.subheader('Colors', divider='rainbow')
+st.subheader('_Streamlit_ is :blue[Blue] :ocean:')
+st.subheader('_Streamlit_ is :green[Green] :four_leaf_clover:')
+st.subheader('_Streamlit_ is :orange[Orange] :maple_leaf:')
+st.subheader('_Streamlit_ is :violet[Violet] :new_moon_with_face:')
+st.subheader('_Streamlit_ is :red[Red] :cherries:')
+st.subheader('_Streamlit_ is :gray[Gray] :ant:')
+st.subheader('_Streamlit_ is :rainbow[Rainbow] :rainbow:')
